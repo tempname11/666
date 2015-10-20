f47d58164ce54d7a480e56d72b3f6197bc36dc21
Vladimir Semyonov <tempname011@gmail.com>
2015-10-20 12:44:57 +0300

    profile-case-1, more verbose

--- client/profiler.js
+++ client/profiler.js
@@ -5,26 +5,36 @@ import all from './reducers';
 import createStorePlus from './store';
 import rootFromStore from './root';
 
+import { state, actions } from './profile-case-1';
+
 const START_DELAY = 2000;
 const DISPATCH_DELAY = 1000;
 
-// TODO
-const profileState = undefined;
-const profileActions = [];
-
-const store = createStorePlus(all, profileState);
+const store = createStorePlus(all, state);
 const root = rootFromStore(store);
 const rootElement = document.getElementById('content');
 
 ReactDOM.render(root, rootElement);
+console.log(`profiler will wait for ${START_DELAY} ms before starting.`);
 
-const delay = ms => new Promise((resolve, reject) => { setTimeout(resolve, ms); });
+const delay = ms => new Promise(resolve => { setTimeout(resolve, ms); });
 
 delay(START_DELAY).then(() => {
+  console.log('profiler has started.');
   Perf.start();
-  profileActions.forEach(a => store.dispatch(a));
+  console.time('666-dispatch');
+  actions.forEach(a => store.dispatch(a));
+  console.timeEnd('666-dispatch');
   return delay(DISPATCH_DELAY);
 }).then(() => {
+  console.log('profiler has finished.');
   Perf.stop();
+  console.log('Inclusive:');
+  Perf.printInclusive();
+  console.log('Exclusive:');
+  Perf.printExclusive();
+  console.log('Wasted:');
   Perf.printWasted();
+  console.log('DOM:');
+  Perf.printDOM();
 });
