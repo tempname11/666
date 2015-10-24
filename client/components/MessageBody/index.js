605c129fef79c54e929227c8b37de981570d19d3
Vladimir Semyonov <tempname011@gmail.com>
2015-10-24 20:25:03 +0300

    MessageBody PureRenderMixin

--- client/components/MessageBody/index.js
+++ client/components/MessageBody/index.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import md from '../../../common/md';
 import emoji from '../../../common/emoji';
 
-export default class extends Component {
-  render() {
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  render: function() {
     const { text } = this.props;
     const htmlString = md(emoji(text));
     return (
       <article className="md"
         dangerouslySetInnerHTML={{ __html: htmlString }} />
     );
-  }
-}
+  },
+});
 
