af65ba85cb0d126418cbedfb0715a2303461f240
Vladimir Semyonov <tempname011@gmail.com>
2015-10-25 14:03:39 +0300

    'many-messages' update for measurements

--- client/profile-scenarios/many-messages.js
+++ client/profile-scenarios/many-messages.js
@@ -2,18 +2,18 @@ import _ from 'lodash';
 import { baseActions, roomID } from './base';
 import { newMessage } from '../actions';
 
-const N = 1000;
+const N = 100;
 const manyMessages = _.range(N).map(i =>
   newMessage({
     roomID,
     userID: 'fake-userID-01',
     messageID: `fake-messageID-${i}`,
     text: `# ${i}.`,
-    time: 1000000000000,
+    time: 1000000000000 + i,
   })
 );
 
-export const delay1 = 3000;
+export const delay1 = 5000;
 export const delay2 = 1000;
 
 export const state = undefined;
@@ -23,5 +23,15 @@ export const preActions = ({history}) => [
   ...manyMessages,
 ];
 
+const makeHorse = i =>
+  newMessage({
+    roomID,
+    userID: 'fake-userID-02',
+    messageID: `fake-messageID-Horse${i}`,
+    text: `Horse #${i}: https://twitter.com/horse_js/status/658139754542710784`,
+    time: 1000007000000 + i,
+  });
+
 export const actions = [
+  makeHorse(0),
 ];
